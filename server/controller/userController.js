4e1784870b5803e13a803c5a55ec705d45a77f29
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-19 06:33:21 -0800

    Created userController in

---
+++ server/controller/userController.js
@@ -0,0 +1,64 @@
+import { User } from "../model/userModel";
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
+import cookieParser from "cookie-parser";
+
+
+// Register User
+export const register = async (req, res)=>{
+    try {
+        const {email, password, name} = req.body;
+        if(!email || !password || !name){
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            })
+        }
+
+        // check is user exixts??
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({
+                success: false,
+                message: "User already exists"
+            })
+        }
+
+        // hash the password
+        const hashPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            name,
+            email,
+            password: hashPassword
+        })
+
+        // generate the token
+        const token = jwt.sign({id: user._id}, process.env.SECRET_KEY, {expiresIn: "7d"})
+        
+        // store the token in cookie
+        res.cookie('token', token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "Strict",
+            maxAge: 7 * 24 * 60 *60 * 1000
+        })
+
+        res.status(200).json({
+            success: true,
+            message: "User register successfully",
+            user: {
+                name: user.name,
+                email: user.email
+            },
+            token
+        })
+
+        
+        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
