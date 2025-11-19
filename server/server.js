6d7c893d75e2157776097f52de54dfa4100b8f5a
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-19 05:22:06 -0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express, { urlencoded } from "express"
+import "dotenv/config"
+import cors from "cors"
+import { dbConnect } from "./config/db.js"
+
+const app = express()
+const port = process.env.PORT || 8084
+
+// Middleware
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/", (req, res)=>{
+    return res.send("Hello this is running")
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running ${port}`);
+    dbConnect()
+    
+})
