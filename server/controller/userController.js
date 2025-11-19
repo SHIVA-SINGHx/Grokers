31d2e8f91bd510b0cc21a9c56b857aab50454c30
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-19 07:00:42 -0800

    created login route

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -72,7 +72,7 @@ export const login = async (req, res) => {
       });
     }
     // check user is exists?
-    const user = await User.find({ email });
+    const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({
         success: false,
