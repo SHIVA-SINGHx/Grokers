028905007224d7537c002faf339d3bb1daebe11e
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-20 10:33:51 -0800

    Created addProduct function

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -1,13 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import { Product } from "../model/productModel";
 
-// create product
-export const createProduct = async(req, res)=>{
+
+export const addProduct = async (req, res) =>{
     try {
-        const {name, description, price, inStock, category} = req.body;
+        const {name, description, category, price, offerPrice} = req.body;
+        const images = req.files;
+        const imageUrl = await Promise.all(
+            images.map(async (item)=>{
+                const result = await cloudinary.uploader(item.path, {
+                    resource_type: "image",
+                })
+                return result.secure_url;
+            })
+        )
+
+        const product = await Product.create({
+            name,
+            description,
+            category,
+            price,
+            offerPrice,
+            productImg: imageUrl
+        })
         
     } catch (error) {
         return res.status(500).json({
             success: false,
-            message: error.message
+            message: "Internal server error"
         })
     }
 }
