8527ebea8ecc148ab2ec3a9d727d023eb4fa03e6
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-21 06:27:22 -0800

    Created getAddress

--- server/controller/addressController.js
+++ server/controller/addressController.js
@@ -7,12 +7,31 @@ export const addAddress = async(req, res)=>{
         const {address} = req.body;
         await Address.create({
             ...address,
-            userId
+            userId,
         })
         return res.status(201).json({
             success: true,
             message: "Address added successfully"
 ,        })
+        
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        })
+    }
+}
+
+// get address
+export const getAddress = async (req, res)=>{
+    try {
+        const userId = req.user;
+        const addresses = (await Address.find({userId})).toSorted({createdAt: -1});
+        return res.status(200).json({
+            success: true,
+            message: "Addresses fetched successfully",
+            addresses,
+        })
         
     } catch (error) {
         return res.status(500).json({
