91d367cfe823dcf84a604b12a099bb809d408fcc
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-21 06:51:16 -0800

    Setup client

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-400 h-screen'>
+      Hello this is Grokers client
+    </div>
+  )
+}
+
+export default App
