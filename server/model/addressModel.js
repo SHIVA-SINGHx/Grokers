6abda28f2ca7cd648c45ee789b0407fffd8b1884
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-21 03:53:57 -0800

    Created addressModel

---
+++ server/model/addressModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email:{type: String, required: true},
+    street:{type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    zipCode: {type: Number, required: true},
+    country: {type: String, required: true},
+    phoneNumber: {type: String, required: true}
+}, {timestamps: true} );
+
+export const Address = mongoose.model("Address", addressSchema);
+
+
