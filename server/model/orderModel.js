00cd1fc247a4517c3fe41185ef5ebef7a4068639
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-21 03:46:33 -0800

    Created orderSchema

---
+++ server/model/orderModel.js
@@ -0,0 +1,47 @@
+import moongoose from 'mongoose';
+
+const orderSchema = new moongoose.Schema({
+    userId:{
+        type: moongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    itemes: [
+        {
+            productId:{
+                type: moongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true
+            },
+            quantity:{
+                type: Number,
+                required: true,
+                default: 1
+            }
+        }
+    ],
+    amount:{
+        type: Number,
+        required: true
+    },
+    address: {
+        type: moongoose.Schema.Types.ObjectId,
+        ref: "Address",
+        required: true
+    },
+    status:{
+        type: String,
+        default: "order placed"
+    },
+    paymentType:{
+        type: String,
+        required: true
+    },
+    isPaid:{
+        type: Boolean,
+        default:  false
+    }
+
+}, {timestamps: true})
+
+export const Order = moongoose.model("Order", orderSchema);
