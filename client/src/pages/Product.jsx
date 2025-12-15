e54ab668719d3a46ef0bc4d9a2d946d8b462a83b
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-12-15 05:54:38 -0800

    Changes App.jsx and added modals

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../components/ProductCart.jsx";
 import { useAppContext } from "../context/AppContext.jsx";
 
 const Products = () => {
--- client/src/pages/SingleProduct.jsx
+++ client/src/pages/SingleProduct.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useAppContext } from "../context/AppContext.jsx";
 import { Link, useParams } from "react-router-dom";
 import { assets } from "../assets/assets";
-import ProductCard from "../components/ProductCard";
+import ProductCard from "../components/ProductCart.jsx";
 const SingleProduct = () => {
   const { products, navigate, addToCart } = useAppContext();
   const { id } = useParams();
