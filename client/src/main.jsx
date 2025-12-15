3aa9483e8f4e777bd5cab9c1eadebe4198a2ba95
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-12-15 09:02:45 -0800

    Minor changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,33 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import { AppContextProvider } from "./context/AppContext.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import ProductCart from "./components/ProductCart";
-import Cart from "./pages/Cart";
-import About from "./pages/About.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
-    ),
-    children: [
-      { index: true, element: <Home /> },
-      { path: "products", element: <ProductCart /> },
-      // { path: "product/:id", element: <ProductDetails /> },
-      { path: "cart", element: <Cart /> },
-      { path: "about", element: <About /> },
-    ],
-  },
-]);
-
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
 );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <Category/>
       <BestSeller/>
       <NewsLetter/>
-      <Footer/>
+      {/* <Footer/> */}
     </div>
   );
 };
